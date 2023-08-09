1e8be2db31826b7adba201a0258a39cdb989c651
Feliciotti <feliciotti@hotmail.com>
2023-08-09 12:53:48 -0300

    carrusel de fotos arreglado, listo el css, parcialmente completo el js, falta agregar un setTimeOut. en _vars mixins de display flex renombrados

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,5 +1,14 @@
-//nav
 const header = document.querySelector('.header');
+
+const slideShow = document.querySelector('.carousel-slide'),
+        slides = Array.from(slideShow.children),
+        slideWidth = slides[0].getBoundingClientRect().width, //returns the width size of the first slide
+
+        dotsNav = document.querySelector('.carousel_nav'),
+        dots = Array.from(dotsNav.children);
+
+
+//------ responsive nav function
 window.addEventListener('scroll', () =>{
     const scrollPos = window.scrollY;
     if(scrollPos > 10){
@@ -10,67 +19,87 @@ window.addEventListener('scroll', () =>{
 });
 
 
+//------------ adopciones - carousel
+slides.forEach((slide, index) =>{ // sets the slides one next to each other
+    slide.style.left = slideWidth * index + 'px';
+})
 
-function byId(id) {
+const moveToSlide = (activeSlide, targetSlide) =>{
+    slideShow.style.transform = `translateX(-${targetSlide.style.left})`
+    activeSlide.classList.remove('carousel-slide--active')
+    targetSlide.classList.add('carousel-slide--active')
 
-   return typeof id==="string"?document.getElementById(id):id;
+    console.log(activeSlide);
 }
 
-let index=0
-    timer=null,
-    banner=byId("banner").getElementsByTagName("div"),
-    len=banner.length,
-    dots=byId("dots").getElementsByTagName("span");
+dotsNav.addEventListener('click', e =>{
+    const targetDot = e.target.closest('span');
+    if(!targetDot) return; 
+    
+    const activeSlide = slideShow.querySelector('.carousel-slide--active')
+    const activeDot = dotsNav.querySelector('.carusel-dot--active')
+    const targetIndex = dots.findIndex(dot => dot === targetDot)
+    const targetSlide = slides[targetIndex]
+
+    moveToSlide(activeSlide, targetSlide)
+
+    activeDot.classList.remove('carusel-dot--active')
+    targetDot.classList.add('carusel-dot--active')
+
+});
+
+// function byId(id) {
+//    return typeof id==="string"?document.getElementById(id):id;
+// }
+
+// let index=0
+//     timer=null,
+//     banner=byId("banner").getElementsByTagName("div"),
+//     len=banner.length,
+//     dots=byId("dots").getElementsByTagName("span");
 
 
-function slideImg() {
+// function slideImg() {
+//     let container = byId("carrusel_container");
+//     container.onmouseover = function () {
+//         if(timer){
+//             clearInterval(timer);
+//         }
+//     }
+//     container.onmouseout = function () {
+//         timer = setInterval(function () {
+//             index++;
+//             if(index>=len){
+//                 index=0;
+//             }
+//             changeImg();
+//         },4000);
+//     };
 
-    let container = byId("carrusel_container");
+//     container.onmouseout();
 
-    container.onmouseover = function () {
-        if(timer){
-            clearInterval(timer);
-        }
-    }
-    container.onmouseout = function () {
-
-        timer = setInterval(function () {
-            index++;
-
-            if(index>=len){
-                index=0;
-            }
-
-            changeImg();
-        },4000);
-    };
-
-    container.onmouseout();
-
-    for(let d = 0; d < len ; d++){
+//     for(let d = 0; d < len ; d++){
         
-        dots[d].id = d;
+//         dots[d].id = d;
 
-        dots[d].onclick=function () {
-            index=this.id;
-            this.className="active";
-            changeImg();
-        };
-    };
+//         dots[d].onclick=function () {
+//             index=this.id;
+//             this.className="active";
+//             changeImg();
+//         };
+//     };
 
-}
+// }
 
-function changeImg() {
-
-    for(let i=0; i<len; i++){
-        banner[i].style.display='none';
-        dots[i].className="";
-    };
-
-    banner[index].style.display='block';
-    dots[index].className="active";
-}
+// function changeImg() {
+//     for(let i=0; i<len; i++){
+//         banner[i].style.display='none';
+//         dots[i].className="";
+//     };
+//     banner[index].style.display='flex';
+//     dots[index].className="active";
+// }
 
 
-slideImg();
+// slideImg();
 
